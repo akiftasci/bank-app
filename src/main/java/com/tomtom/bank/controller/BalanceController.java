bbca7b723996bfd354b40c729830cb3d3b588a11
akiftascidev <akif@lincsafe.com>
2021-11-28 15:46:36 +0100

    Implement balance

--- src/main/java/com/tomtom/bank/controller/BalanceController.java
+++ src/main/java/com/tomtom/bank/controller/BalanceController.java
@@ -1,4 +1,22 @@
 package com.tomtom.bank.controller;
 
+import com.tomtom.bank.dto.BalanceWrapperDto;
+import com.tomtom.bank.service.BalanceService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BalanceController {
+    private final BalanceService balanceService;
+
+    public BalanceController(final BalanceService balanceService) {
+        this.balanceService = balanceService;
+    }
+
+    @PostMapping(value = "accounts/{id}/balance")
+    public BalanceWrapperDto balance(@PathVariable Long id) {
+        BalanceWrapperDto balanceWrapperDto = balanceService.getBalance(id);
+        return balanceWrapperDto;
+    }
 }
--- src/main/java/com/tomtom/bank/service/BalanceService.java
+++ src/main/java/com/tomtom/bank/service/BalanceService.java
@@ -1,4 +1,27 @@
 package com.tomtom.bank.service;
 
+import com.tomtom.bank.dto.BalanceDto;
+import com.tomtom.bank.dto.BalanceWrapperDto;
+import com.tomtom.bank.entity.Account;
+import com.tomtom.bank.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BalanceService {
+    private final AccountRepository accountRepository;
+
+    public BalanceService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public BalanceWrapperDto getBalance(final Long id) {
+        final BalanceDto balanceDto = new BalanceDto();
+        final BalanceWrapperDto balanceWrapperDto = new BalanceWrapperDto();
+
+        final Account account = accountRepository.findById(id).get();
+        final float balance = account.getBalance();
+        balanceDto.setAmount(balance);
+        balanceWrapperDto.setBalanceDto(balanceDto);
+        return balanceWrapperDto;
+    }
 }
