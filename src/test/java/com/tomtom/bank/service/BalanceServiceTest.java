bad18834543990a531cc6538ad8de7024c5371fa
akiftascidev <akif@lincsafe.com>
2021-11-30 22:26:55 +0100

    Add balance service test

---
+++ src/test/java/com/tomtom/bank/service/BalanceServiceTest.java
@@ -0,0 +1,43 @@
+package com.tomtom.bank.service;
+
+import com.tomtom.bank.dto.BalanceDto;
+import com.tomtom.bank.dto.BalanceWrapperDto;
+import com.tomtom.bank.entity.Account;
+import com.tomtom.bank.repository.AccountRepository;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BalanceServiceTest {
+
+    private BalanceService balanceServiceMock;
+    private AccountRepository accountRepositoryMock;
+
+    @Before
+    public void setUp(){
+        accountRepositoryMock = mock(AccountRepository.class);
+        balanceServiceMock = new BalanceService(accountRepositoryMock);
+    }
+
+    @Test
+    public void testWithdrawal(){
+        when(accountRepositoryMock.findById(1L)).thenReturn(createAccount());
+        final BalanceWrapperDto withdrawalResponse = balanceServiceMock.getBalance(1L);
+        final BalanceDto balance = withdrawalResponse.getBalance();
+
+        Assertions.assertThat(balance.getAmount()).isEqualTo(300);
+    }
+
+    private Optional<Account> createAccount() {
+        final Account account = new Account();
+        account.setId(1L);
+        account.setName("akif");
+        account.setLimits(200);
+        account.setBalance(300);
+        return Optional.of(account);
+    }
+}
